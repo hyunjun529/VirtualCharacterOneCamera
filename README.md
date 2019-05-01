57d9a6a93fd96f8d62349a0c931f1a9b9e99e78c
hyunjun529 <hyunjun529@gmail.com>
2019-05-01 15:45:16 +0900

    목표 좀 더 단순하게 하향

--- README.md
+++ README.md
@@ -8,28 +8,30 @@
 - [x] 좌우반전
 - [x] 입력 소스를 조금 더 크게
 - [x] WebCam image 투사 지우기
-- [ ] **끊어진 프레임 사이에서 Interpolation**
-- [ ] Debug Parameter 출력하기
-- [ ] config 만들기
+- [X] **끊어진 프레임 사이에서 Interpolation**
+- [X] Debug Parameter 출력하기
+- [X] config 만들기
 - [ ] **Scale값에 따른 3D 공간으로 변환**
-- [ ] 모니터 위를 볼 때 와 아래를 볼 때 구분하기
-- [ ] OpenFace로 dlib, Face Landmark
+- [ ] ~~OpenFace로 dlib, Face Landmark를 대체하기~~
+- [ ] ~~모니터 위를 볼 때 와 아래를 볼 때 구분하기~~
 
 ![featurePoint](resource/facemarks_points.png)
 
 ### Python OpenVR
-- [ ] OpenVR로 HMD 빼고 컨트롤러 위치만 가져오기
+- [ ] ~~OpenVR로 HMD 빼고 컨트롤러 위치만 가져오기~~
 
 ### Python ~ Unity Socket
 - [ ] Untiy로 Python에서 값을 전달한다.
 
 ### Unity Character Setup
-- [ ] 
-- [ ] FeaturePoint들과 캐릭터를 동기화
+- [ ] 캐릭터 Scene 구현 
+- [ ] 기본 어플리케이션 구현
+- [ ] Face Landmark와 캐릭터 표정 맞추기
+- [ ] ~~OpenVR Controller와 손 위치 맞추기~~
 
 
 ## Spec
-1. Python, OpenCV, dlib으로 Face Landmark 받기
+Face Landmark > VRoid Character
 
 ## Example (WIP)
 
@@ -41,5 +43,7 @@
 
 
 ## reference
-[dlib-model : shape_predictor_68_face_landmarks](https://github.com/davisking/dlib-models)
-[ValveSoftware/openvr](https://github.com/ValveSoftware/openvr)
+- [dlib-model : shape_predictor_68_face_landmarks](https://github.com/davisking/dlib-models)
+- [OpenFace](https://github.com/TadasBaltrusaitis/OpenFace)
+- [ValveSoftware/openvr](https://github.com/ValveSoftware/openvr)
+- [pyopenvr](https://github.com/cmbruns/pyopenvr)
--- run.py
+++ run.py
@@ -1,3 +1,92 @@
-import py.FaceLandmark
+import cv2
+import numpy as np
+import dlib
+import time
 
-FaceLandmark()
+# Properties는 아래 링크에서 확인
+# https://docs.opencv.org/4.1.0/d4/d15/group__videoio__flags__base.html#gaeb8dd9c89c10a5c63c139bf7c4f5704d
+cap = cv2.VideoCapture(0)
+fps = cap.get(5)
+
+# https://github.com/davisking/dlib-models
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+prevTimestamp = 0
+currentTimestamp = 0
+currentRectangle = np.ndarray(shape=(2, 2), dtype=int)
+currentLandmarks = np.ndarray(shape=(68, 2), dtype=int)
+
+renderRectangle = np.ndarray(shape=(2, 2), dtype=int)
+renderLandmarks = np.ndarray(shape=(68, 2), dtype=int)
+
+# interpolation
+flagNeedToUpdate = False
+
+while True:
+    _, frame = cap.read()
+
+    # 초기화
+    flagNeedToUpdate = False
+
+    # frame 크기
+    screenWidth = int(round(cap.get(4)))
+    screenHeight = int(round(cap.get(3)))
+
+    # 좌우 반전
+    flipedFrame = frame.copy()
+    flipedFrame = cv2.flip(frame, 1)
+
+    # 출력용 화면
+    blankImage = np.zeros((screenWidth, screenHeight, 3), np.uint8)
+
+    # predictor에 넣을 프레임
+    targetFrame = flipedFrame
+    
+    # 화면에 출력될 프레임
+    renderFrame = blankImage
+
+    # Face predictor
+    gray = cv2.cvtColor(targetFrame, cv2.COLOR_BGR2GRAY)
+    faces = detector(gray)
+    for face in faces:
+        currentRectangle[0][0] = face.left()
+        currentRectangle[0][1] = face.top()
+        currentRectangle[1][0] = face.right()
+        currentRectangle[1][1] = face.bottom()
+        
+        landmarks = predictor(gray, face)
+        
+        flagNeedToUpdate = True
+
+        for n in range(0, 68):
+            currentLandmarks[n][0] = landmarks.part(n).x
+            currentLandmarks[n][1] = landmarks.part(n).y
+        
+    # Interpolation
+    currentTimestamp = time.time()
+    interTimestamp = (currentTimestamp - prevTimestamp) * 1000 / fps
+    renderRectangle = renderRectangle + (currentRectangle - renderRectangle) / interTimestamp
+    renderLandmarks = renderLandmarks + (currentLandmarks - renderLandmarks) / interTimestamp
+    prevTimestamp = currentTimestamp
+
+    # Rendering
+    renderRectangle = renderRectangle.astype(int)
+    renderLandmarks = renderLandmarks.astype(int)
+
+    x1 = renderRectangle[0][0]
+    y1 = renderRectangle[0][1]
+    x2 = renderRectangle[1][0]
+    y2 = renderRectangle[1][1]
+    cv2.rectangle(renderFrame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+
+    for n in range(0, 68):
+        x = renderLandmarks[n][0]
+        y = renderLandmarks[n][1]
+        cv2.circle(renderFrame, (x, y), 2, (255, 0, 0), -1)
+
+    cv2.imshow("Frame", renderFrame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
